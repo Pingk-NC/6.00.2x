1ca01537c365d807df4e7beae9ccdd66cb55aea2
Pingk-NC <iamnickesh@gmail.com>
2018-06-01 16:51:14 -0400

    Single bag solution

--- Ex01.py
+++ Ex01.py
@@ -13,6 +13,10 @@ def powerSet(items):
 
 items = ['1', '10', '100', '1,000', '10,000']
 
+result = powerset(items)
+for i in range(2**N):
+    print(next(result))
+
 def yieldAllCombos(items):
     """
     Generates all combinations of N items into two bags, whereby each item is in either zero or one bags.
@@ -20,20 +24,13 @@ def yieldAllCombos(items):
     Yields a tuple, (bag1, bag2), where each bag is a list of the items in that bag, ([], [])
     """
     N = len(items)
-    for i in range(2**N):
+    for i in range(3**N):
         bag1 = []
         bag2 = []
-        for j in range(N):
-            if (i >> j) % 2 == 1:
-                bag1.append(items[j])
+        for j in range
         
-        M = len(bag1)
-        if i == 0:
-            for j in range(N):
-                bag2.append(items[j])
-        yield (bag1, bag2)
 
-result = yieldAllCombos(items)
-for i in range(2**len(items)):
-    print(next(result))
+#result = yieldAllCombos(items)
+#for i in range(2**len(items)):
+#    print(next(result))
 
