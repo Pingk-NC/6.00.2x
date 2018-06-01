6a7ba7f4b181102d2979c120b12be82d21ab83f5
Pingk-NC <iamnickesh@gmail.com>
2018-06-01 17:27:20 -0400

    Two bag solution

--- Ex01.py
+++ Ex01.py
@@ -13,9 +13,9 @@ def powerSet(items):
 
 items = ['1', '10', '100', '1,000', '10,000']
 
-result = powerSet(items)
-for i in range(2**len(items)):
-    print(next(result))
+#result = powerSet(items)
+#for i in range(2**len(items)):
+#    print(next(result))
 
 def yieldAllCombos(items):
     """
@@ -26,11 +26,26 @@ def yieldAllCombos(items):
     N = len(items)
     for i in range(3**N):
         bag1 = []
-        bag2 = []
-        #for j in range
-        
+        for j in range(N):
+            if (i >> j) % 2 == 1:
+                bag1.append(items[j])
+        #Outside the j loop, we know a single bag1 combination
+        #We need to look in bag one, make a subset of (items) that does not include items in (bag1), since duplicates are not allowed, and create the powerSet for bag 2 given bag 1.
 
-#result = yieldAllCombos(items)
-#for i in range(2**len(items)):
-#    print(next(result))
+        itemSubset = items.copy()
+        if bag1 != []:
+            for object in bag1:
+                itemSubset.remove(object)
+        
+        M = len(itemSubset)
+        for i in range(2**M):
+            bag2 = []
+            for j in range(N):
+                if (i >> j) % 2 == 1:
+                    bag2.append(itemSubset[j])
+            yield (bag1, bag2)
+
+result = yieldAllCombos(items)
+for i in range(3**len(items)):
+    print(next(result))
 
